14b239fc383e842519e87b586bc05369b63a23be
VICTORdecreator <victordecreator@gmail.com>
2026-07-01 13:15:15 +0100

    update

--- Assignment1 .cpp
+++ Assignment1 .cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main() {
     cout << "IKEH VICTOR AKACHUKWU" << endl;
     cout << "25/EG/CO/058" << endl;
-    cout << "COMPUTER ENGINEERING" <<endl;
+    cout << "COMPUTER ENGINEERING " <<endl;
+    cout << "UNIVERSITY OF UYO" << endl;
     return 0;
 }
